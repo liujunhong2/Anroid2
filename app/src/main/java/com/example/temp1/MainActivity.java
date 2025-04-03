b8c8db6cf37837ddad7dbb2fd777601e9082f631
liujunhong2 <2499185166@qq.com>
2025-04-03 15:34:17 +0800

    generate score board

--- app/src/main/java/com/example/temp1/MainActivity.java
+++ app/src/main/java/com/example/temp1/MainActivity.java
@@ -16,48 +16,76 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText input1;
-    private Button button1;
-    private TextView text2;
-    private EditText input2;
-
+    private Button add1, add2, add3, reset1;
+    private TextView out1;
+    int cnt1 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        input1 = findViewById(R.id.input_weight);
-        input2 = findViewById(R.id.input_height);
-        button1 = findViewById((R.id.button1));
-        text2 = findViewById(R.id.text_BMI);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        add1 = findViewById((R.id.add1));
+        add2 = findViewById((R.id.add2));
+        add3 = findViewById((R.id.add3));
+        reset1 = findViewById(R.id.reset1);
+        out1 = findViewById(R.id.out1);
+
+
+        add1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                String in1 = input1.getText().toString(), in2 = input2.getText().toString();
-                double w1 = Double.parseDouble(in1), h1 = Double.parseDouble(in2);
-                double bmi = w1 / pow(h1, 2);
-                String temp = ", ";
-                if(bmi < 15){
-                    temp += "too light";
-                }else if(bmi < 16){
-                    temp += "middle light";
-                }else if(bmi < 18.5){
-                    temp += "little light";
-                }else if(bmi < 25){
-                    temp += "normal";
-                }else if(bmi < 30){
-                    temp += "little fat";
-                }else if(bmi < 35){
-                    temp += "fat 1 level";
-                }else if(bmi < 40){
-                    temp += "fat 2 level";
-                }else{
-                    temp += "fat 3 level";
+
+                String now = (String) out1.getText();
+                if(cnt1 == 0){
+                    now = "0";
                 }
-                String ans = "BMI: " + String.format("%.2f",bmi) + temp;
-                text2.setText(ans);
+                cnt1 ++;
+                int s_n = Integer.parseInt(now);
+                s_n += 1;
+                String ans = String.format("%d", s_n);
+                out1.setText(ans);
             }
         });
+
+        add2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String now = (String) out1.getText();
+                if(cnt1 == 0){
+                    now = "0";
+                }
+                cnt1 ++;
+                int s_n = Integer.parseInt(now);
+                s_n += 2;
+                String ans = String.format("%d", s_n);
+                out1.setText(ans);
+            }
+        });
+
+        add3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String now = (String) out1.getText();
+                if(cnt1 == 0){
+                    now = "0";
+                }
+                cnt1 ++;
+                int s_n = Integer.parseInt(now);
+                s_n += 3;
+                String ans = String.format("%d", s_n);
+                out1.setText(ans);
+            }
+        });
+
+        reset1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                out1.setText("0");
+                cnt1 ++;
+            }
+        });
+
     }
 }
